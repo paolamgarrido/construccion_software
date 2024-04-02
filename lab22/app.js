2a5df30abaef2022021df412a50513fda9cc91a9
Paola Garrido <111533069+paolamgarrido@users.noreply.github.com>
2024-04-01 19:47:03 -0600

    add lab 22

---
+++ lab22/app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Middleware
+app.use((request, response, next) => {
+  console.log('Middleware!');
+  next();
+});
+
+const session = require('express-session');
+
+app.use(session({
+    secret: 'mi string secreto que debería ser muy largo', 
+    resave: false, 
+    saveUninitialized: false,
+}));
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+// Multer
+
+const multer = require('multer');
+//fileStorage: constante de configuración para manejar el almacenamiento
+const fileStorage = multer.diskStorage({
+    destination: (request, file, callback) => {
+        //'public/uploads': directorio del servidor donde se subirán los archivos 
+        callback(null, 'public/uploads');
+    },
+    filename: (request, file, callback) => {
+        // nombre que queremos que tenga el archivo en el servidor + timestamp
+        callback(null, Number(new Date()).toString() + file.originalname);
+    },
+});
+app.use(multer({ storage: fileStorage }).single('imagen')); 
+
+
+//Protección contra ataques de CSRF
+const csrf = require('csurf');
+const csrfProtection = csrf();
+app.use(csrfProtection); 
+
+const rutaClases= require('./routes/clases.routes');
+app.use('/home',rutaClases); 
+
+const rutaUsuarios = require('./routes/login.routes');
+app.use(rutaUsuarios); 
+
+const rutaJoyas= require('./routes/joyas.routes');
+app.use(rutaJoyas); 
+
+app.use((request, response, next) => {
+  response.status(404);
+  response.sendFile(path.join(__dirname,'views','404.html')); //Manda la respuesta
+});
+
+app.listen(4000);
---
+++ lab22/controllers/joyas.controller.js
@@ -0,0 +1,49 @@
+const Joya = require('../models/joya.model');
+
+exports.get_joyas = (request, response, next) => {
+    response.render('creajoya', {
+        username: request.session.username || '',
+        csrfToken: request.csrfToken(),
+    });
+};
+
+exports.post_joyas = (request, response, next) => {
+    console.log(request.body);
+    console.log(request.file);
+    const mi_joya = new Joya(
+        request.body.marca, 
+        request.body.nombre,
+        request.body.tipo, 
+        request.body.material,
+        request.body.piedra, 
+        request.body.peso,
+        //imagen
+        request.file.filename
+    );
+    mi_joya.save()
+        .then(([rows, fieldData]) => {
+            response.setHeader('Set-Cookie', 'ultima_joya=' + mi_joya.clase + '; HttpOnly');
+            response.redirect('/joyas');
+        }).catch((error) => {
+            console.log(error);
+        });
+};
+
+exports.get_misjoyas = (request, response, next) => {
+    console.log(request.cookies);
+    console.log(request.cookies.ultima_joya);
+
+    //Joya.fetchAll().then(([rows, fieldData]) => { 
+        //console.log(fieldData);
+        Joya.fetch(request.params.id_producto).then(([rows, fieldData]) => {
+        response.render('misjoyas', {
+            joyas: rows,
+            ultima_joya: request.cookies.ultima_joya || '',
+            username: request.session.username || '',
+        });
+    })
+    .catch((error) => {
+        console.log(error)
+    });
+
+};
